458bd8fbace20c2d3817bc8e29373d2b0e99903e
Joao Mateus <joao.amaral@ifam.edu.br>
2023-03-07 14:33:32 -0300

    passing data via props

--- ReactBasics/first-project/src/App.js
+++ ReactBasics/first-project/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { ExpenseItem } from './components/ExpenseItem';
+import { Expenses } from './components/Expenses';
 
 function App() {
   const mockedData = [
@@ -11,10 +11,7 @@ function App() {
   return (
     <div className="App">
       <div className='main-div'>
-        <ExpenseItem title={mockedData[0].title} amount={mockedData[0].amount} date={mockedData[0].date} /> 
-        <ExpenseItem title={mockedData[1].title} amount={mockedData[1].amount} date={mockedData[1].date} /> 
-        <ExpenseItem title={mockedData[2].title} amount={mockedData[2].amount} date={mockedData[2].date} /> 
-        <ExpenseItem title={mockedData[3].title} amount={mockedData[3].amount} date={mockedData[3].date} /> 
+        <Expenses expenses={mockedData} />
       </div>
     </div>
   );
--- ReactBasics/first-project/src/components/ExpenseItem.js
+++ ReactBasics/first-project/src/components/ExpenseItem.js
@@ -4,9 +4,9 @@ import './ExpenseItem.css'
 export const ExpenseItem = ({title, amount, date}) => {
   return (
     <div className="expense-item">
-      <div>{title}</div>
-      <div className="expense-item__description">
         <h2>{date}</h2>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
         <div className="expense-item__price">R${amount}</div>
       </div>
     </div>
