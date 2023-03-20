94a332d92eae9b5ae96136608b7ef854e10e147e
Joao Mateus <joao.amaral@ifam.edu.br>
2023-03-20 18:29:58 -0300

    map instead of copy paste expense item

--- 3-ConditionalRendering/first-project-part-3/src/components/Expenses/Expenses.jsx
+++ 3-ConditionalRendering/first-project-part-3/src/components/Expenses/Expenses.jsx
@@ -14,10 +14,13 @@ export const Expenses = ({expenses}) => {
     <>
       <Card className='expenses'>
         <ExpensesFilter selected={filter} expenseFilterListener={handleFilterValue} />
-        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}  />
-        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}  />
-        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}  />
-        <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}  />
+        {
+          expenses.map(expense => {
+            return (
+              <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} />
+            )
+          })
+        }
       </Card>
     </>
   )
